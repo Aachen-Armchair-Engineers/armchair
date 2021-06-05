b6d4815043e4c1482ea0304d5541d6239c12ad61
Felix Reuter <felix.reuter.fr@gmail.com>
2021-06-05 21:07:40 +0000

    Now reads the data from yolov4 from depthai-ros-examples

--- scripts/receiver.py
+++ scripts/receiver.py
@@ -2,14 +2,30 @@
 
 import rospy
 from std_msgs.msg import String
+from depthai_ros_msgs.msg import SpatialDetectionArray
+
+labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train",
+          "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+          "bird", "cat", "dog", "horse", "sheep", "cow", "elephant",
+          "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
+          "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+          "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+          "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich",
+          "orange", "broccoli",   "carrot", "hot dog", "pizza", "donut", "cake",
+          "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor",
+          "laptop", "mouse", "remote", "keyboard", "cell phone",  "microwave", "oven",
+          "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+          "teddy bear", "hair drier", "toothbrush"]
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + ": %s", data.data)
+    for detection in data.detections:
+        rospy.loginfo("%s (%2f, %2f, %2f)", labels[detection.results[0].id], detection.position.x, detection.position.y, detection.position.z)
 
 def listener():
     rospy.init_node('receiver', anonymous=True)
 
-    rospy.Subscriber("mockdata", String, callback)
+    rospy.Subscriber("/yolov4_publisher/color/yolov4_Spatial_detections", SpatialDetectionArray, callback)
+    #rospy.Subscriber("/yolov4_publisher/color/yolov4_Spatial_detections", String, callback)
 
     rospy.spin()
 
