66d799f4263a81e4bdde8d3af594f84352e8fd0a
Felix Reuter <felix.reuter@rwth-aachen.de>
2021-06-14 15:19:32 +0200

    Now publishes the position so its visible in rviz

--- scripts/receiver.py
+++ scripts/receiver.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-import rospy
-from std_msgs.msg import String
+import rospy, tf2_ros
+from std_msgs.msg import String, Header
+from geometry_msgs.msg import Point, PointStamped
 from depthai_ros_msgs.msg import SpatialDetectionArray
 
 labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train",
@@ -17,17 +18,29 @@ labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train",
           "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
           "teddy bear", "hair drier", "toothbrush"]
 
+pub = rospy.Publisher('armchair/door_position', PointStamped, queue_size=10)
+
 def callback(data):
     for detection in data.detections:
-        rospy.loginfo("%s (%2f, %2f, %2f)", labels[detection.results[0].id], detection.position.x, detection.position.y, detection.position.z)
+        if detection.results[0].id == 0:
+            rospy.loginfo("%s (%2f, %2f, %2f)", labels[detection.results[0].id], detection.position.x, detection.position.y, detection.position.z)
+            point_stamped = PointStamped()
+            point_stamped.header.stamp = rospy.Time.now()
+            point_stamped.header.frame_id = "oak-d_frame"
+            point_stamped.point = Point(detection.position.x, detection.position.y, detection.position.z)
+            pub.publish(point_stamped)
+            #rate.sleep()
 
 def listener():
     rospy.init_node('receiver', anonymous=True)
-
+    rate = rospy.Rate(10)
     rospy.Subscriber("/yolov4_publisher/color/yolov4_Spatial_detections", SpatialDetectionArray, callback)
-    #rospy.Subscriber("/yolov4_publisher/color/yolov4_Spatial_detections", String, callback)
 
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        pass
+
