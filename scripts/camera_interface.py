37a186a31464fc222d1d32c966c21791573f8602
Felix Reuter <felix.reuter@rwth-aachen.de>
2021-08-06 15:10:58 +0200

    Now using the launch parameter for the python node aswell

--- scripts/camera_interface.py
+++ scripts/camera_interface.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from numpy.lib.type_check import mintypecode
 import rospy, tf2_ros
 from std_msgs.msg import String, Header
 from geometry_msgs.msg import Point, PointStamped
@@ -11,67 +12,73 @@ from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import ImageMarker
 from foxglove_msgs.msg import ImageMarkerArray
 
-############################################################################
-#WHEN CHANGING THE NN ALSO CHANGE WHICH TOPIC TO SUBSCRIBE TO AT THE BOTTOM#
-############################################################################
+labels = []
+target_label = ""
 
-# labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train",
-#          "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
-#          "bird", "cat", "dog", "horse", "sheep", "cow", "elephant",
-#          "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
-#          "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-#          "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-#          "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich",
-#          "orange", "broccoli",   "carrot", "hot dog", "pizza", "donut", "cake",
-#          "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor",
-#          "laptop", "mouse", "remote", "keyboard", "cell phone",  "microwave", "oven",
-#          "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-#          "teddy bear", "hair drier", "toothbrush"]
-# target_label = "cup"
+def init_labels(model):
+    global labels, target_label
 
-labels = [
-    "background",
-    "aeroplane",
-    "bicycle",
-    "bird",
-    "boat",
-    "bottle",
-    "bus",
-    "car",
-    "cat",
-    "chair",
-    "cow",
-    "diningtable",
-    "dog",
-    "horse",
-    "motorbike",
-    "person",
-    "pottedplant",
-    "sheep",
-    "sofa",
-    "train",
-    "tvmonitor"
-]
-target_label = "bottle"
 
-#labels = ["", "door", "handle", "cabinet door", "refridgerator door"]
-#target_label = "handle"
+    if model == 'armchair':
+        labels = ["", "door", "handle", "cabinet door", "refridgerator door"]
+        target_label = "handle"
 
-pub = rospy.Publisher('/armchair/handle_position', PointStamped, queue_size=10)
+    elif model == 'mobilenet':
+        labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train",
+                 "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+                 "bird", "cat", "dog", "horse", "sheep", "cow", "elephant",
+                 "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
+                 "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                 "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                 "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich",
+                 "orange", "broccoli",   "carrot", "hot dog", "pizza", "donut", "cake",
+                 "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor",
+                 "laptop", "mouse", "remote", "keyboard", "cell phone",  "microwave", "oven",
+                 "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                 "teddy bear", "hair drier", "toothbrush"]
+        target_label = "cup"
+
+    elif model == 'yolov4':
+        labels = [
+            "background",
+            "aeroplane",
+            "bicycle",
+            "bird",
+            "boat",
+            "bottle",
+            "bus",
+            "car",
+            "cat",
+            "chair",
+            "cow",
+            "diningtable",
+            "dog",
+            "horse",
+            "motorbike",
+            "person",
+            "pottedplant",
+            "sheep",
+            "sofa",
+            "train",
+            "tvmonitor"
+        ]
+        target_label = "bottle"
+
+    else:
+        rospy.logerr("Invalid neuronal network selected, aborting")
+        exit(0)
+
+pub_handle = rospy.Publisher('/armchair/handle_position', PointStamped, queue_size=10)
 pub_markers = rospy.Publisher("/mobilenet_node_custom/color/markers", ImageMarkerArray, queue_size=1)
 
 def callback(data):
-
-    #rospy.loginfo(data)
 
     highest_score = 0.0
     point_stamped = PointStamped()
     markers = ImageMarkerArray()
 
+    #TODO: figure out a more pythonic approach for the whole loop
     for d in data.detections:
-
-        #rospy.loginfo(d)
-
         #Find best object
         #Alternatively use the clostest or manually select one
         if labels[d.results[0].id] == target_label:
@@ -83,8 +90,10 @@ def callback(data):
                 point_stamped.point = Point(d.position.x, d.position.z, d.position.y)
                 highest_score = d.results[0].score
         
-        #Create markers for all relevant object
 
+        #Create markers for all relevant object
+        
+        #Calculate the positions of the bounding box edges
         vertex_left = d.bbox.center.x - d.bbox.size_x/2
         vertex_right = d.bbox.center.x + d.bbox.size_x/2
         vertex_top = d.bbox.center.y - d.bbox.size_y/2
@@ -96,6 +105,7 @@ def callback(data):
         vertex_top *= 720/320
         vertex_bot *= 720/320
 
+        #FIXME: Assigning the values in the constructor caused issues before, see if that is fixable
         color=ColorRGBA()
         color.r=0
         color.g=255
@@ -126,7 +136,7 @@ def callback(data):
         )
 
     if highest_score > 0.0:
-        pub.publish(point_stamped)
+        pub_handle.publish(point_stamped)
 
     pub_markers.publish(markers)
             
@@ -136,8 +146,17 @@ def callback(data):
 def listener():
     rospy.init_node('camera_interface', anonymous=True)
     rate = rospy.Rate(10)
-    #rospy.Subscriber("/yolov4_publisher/color/yolov4_Spatial_detections", SpatialDetectionArray, callback)
-    rospy.Subscriber("/mobilenet_node_custom/color/spatial_detections", SpatialDetectionArray, callback)
+
+    model = rospy.get_param("/camera_interface/model")
+
+    #Use the matching labels
+    init_labels(model)
+
+    #And the matching topic
+    if model == "yolov4":
+        rospy.Subscriber("/yolov4_publisher/color/yolov4_Spatial_detections", SpatialDetectionArray, callback)
+    else:
+        rospy.Subscriber("/mobilenet_node_custom/color/spatial_detections", SpatialDetectionArray, callback)
 
     rospy.spin()
 
